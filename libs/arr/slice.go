5eb74042d3ac68a8f6d6617f2dd08d6df306fa43
zhangjiulong <kowloonzh@gmail.com>
2019-06-03 12:10:31 +0800

    master:add arr lib and in array func
    Signed-off-by: zhangjiulong <kowloonzh@gmail.com>

---
+++ libs/arr/slice.go
@@ -0,0 +1,18 @@
+package arr
+
+import "reflect"
+
+// 判断 obj 是否存在于 target 中
+func InArray(obj interface{}, target interface{}) bool {
+	targetValue := reflect.ValueOf(target)
+	switch reflect.TypeOf(target).Kind() {
+	case reflect.Slice, reflect.Array:
+		for i := 0; i < targetValue.Len(); i++ {
+			if targetValue.Index(i).Interface() == obj {
+				return true
+			}
+		}
+	}
+
+	return false
+}
