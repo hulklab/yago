99ebb7ee1380132a7ece0f0bd7c5184a9248c7c4
zhangjiulong <kowloonzh@gmail.com>
2019-08-01 11:07:10 +0800

    f_rds:add redis lib api
    Signed-off-by: zhangjiulong <kowloonzh@gmail.com>

--- libs/rds/redis.go
+++ libs/rds/redis.go
@@ -114,11 +114,11 @@ type Subscriber struct {
 	closeChan chan bool
 }
 
-func NewSubscriber(conn redis.Conn, topic string) (*Subscriber, error) {
+func (r *Rds) NewSubscriber(topic string) (*Subscriber, error) {
 	s := new(Subscriber)
-	s.closeChan = make(chan bool)
+	s.closeChan = make(chan bool, 1)
 	s.topic = topic
-	prc := redis.PubSubConn{Conn: conn}
+	prc := redis.PubSubConn{Conn: r}
 	err := prc.Subscribe(s.topic)
 	if err != nil {
 		log.Println("redis: ", err.Error())
