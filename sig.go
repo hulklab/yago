1c758e9f74fd94e4d28bfe742b40d04d64d1a0a7
zhangjiulong <kowloonzh@gmail.com>
2019-08-19 12:18:02 +0800

    master:support windows
    Signed-off-by: zhangjiulong <kowloonzh@gmail.com>

---
+++ sig.go
@@ -0,0 +1,34 @@
+// +build !windows
+
+package yago
+
+import (
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func (a *App) startSignal() {
+	pid := os.Getpid()
+	signals := make(chan os.Signal, 1)
+	signal.Notify(signals, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+
+	for {
+		s := <-signals
+		log.Println("recv", s)
+
+		switch s {
+		case syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT:
+			log.Println("Graceful Shutdown...")
+
+			a.Close()
+
+			log.Println("Process", pid, "Exit OK")
+			os.Exit(0)
+		case syscall.SIGUSR2:
+			log.Println("Restart...")
+			log.Println("Process", pid, "Restart ok")
+		}
+	}
+}
---
+++ sig_windows.go
@@ -0,0 +1,24 @@
+// +build windows
+
+package yago
+
+func (a *App) startSignal() {
+	pid := os.Getpid()
+	signals := make(chan os.Signal, 1)
+	signal.Notify(signals, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+
+	for {
+		s := <-signals
+		log.Println("recv", s)
+
+		switch s {
+		case syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT:
+			log.Println("Graceful Shutdown...")
+
+			a.Close()
+
+			log.Println("Process", pid, "Exit OK")
+			os.Exit(0)
+		}
+	}
+}
