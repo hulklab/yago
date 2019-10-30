69e505e23999cb72d0d3505c571d26fb01707931
zhangjiulong <kowloonzh@gmail.com>
2019-10-30 11:47:50 +0800

    update

---
+++ build.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+IS_PUSH=""
+
+function build() {
+    rm -rf _book
+    gitbook install
+    gitbook build
+
+    if [[ ! -d "_book" ]];then
+        echo "[ERROR]编译 book 失败"
+        exit 1
+    fi
+}
+
+function deploy() {
+
+    echo "[INFO]发布到 gh-pages 分支，发布后直接生效"
+    if  read -p "确认要发布吗[y|n]，默认为 n: > " IS_PUSH;then
+        if [[ -z ${IS_PUSH} ]];then
+            IS_PUSH="n"
+        fi
+    fi
+
+    if [[ ${IS_PUSH} != "y" ]];then
+        return
+    fi
+
+    rm -rf /tmp/_book
+    mv _book /tmp/_book
+    git checkout gh-pages
+    mv /tmp/_book/* ./
+    git add .
+    git commit -a -m "update"
+    git push origin gh-pages
+}
+
+function main() {
+    build
+    deploy
+}
+
+main
