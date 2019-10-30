8d1423f410827ad56377542d370be5654b2d1379
zhangjiulong <kowloonzh@gmail.com>
2019-10-30 11:06:54 +0800

    doc:add book json
    Signed-off-by: zhangjiulong <kowloonzh@gmail.com>

---
+++ build.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+IS_PUSH=""
+
+function build() {
+    rm -rf _book
+    gitbook install
+    gitbook build
+
+    if [[ ! -d "_book" ]];then
+        echo "[ERROR]编译 book 失败"
+        exit 1
+    fi
+}
+
+function deploy() {
+
+    echo "[INFO]发布到 gh-page 分支，发布后直接生效"
+    if  read -p "确认要发布吗[y|n]，默认为 n: > " IS_PUSH;then
+        if [[ -z ${IS_PUSH} ]];then
+            IS_PUSH="n"
+        fi
+    fi
+
+    if [[ ${IS_PUSH} != "y" ]];then
+        return
+    fi
+
+    rm -rf /tmp/_book
+    cp -r _book /tmp/_book
+    git co gh-page
+    git branch
+}
+
+function main() {
+    build
+    deploy
+}
+
+main
