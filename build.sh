5a56323206016453a6ee364efb7f5d53617acebb
zhangjiulong <kowloonzh@gmail.com>
2019-10-30 14:47:42 +0800

    update

--- build.sh
+++ build.sh
@@ -4,10 +4,14 @@ IS_PUSH=""
 
 function build() {
     rm -rf _book
-    gitbook install
+
+    if [[ ! -d "node_modules" ]];then
+        gitbook install
+    fi
+
     gitbook build
 
-    if [[ ! -d "_book" ]];then
+    if [[ $? != 0 ]] || [[ ! -d "_book" ]];then
         echo "[ERROR]编译 book 失败"
         exit 1
     fi
@@ -26,6 +30,7 @@ function deploy() {
         return
     fi
 
+    git pull
     rm -rf /tmp/_book
     mv _book /tmp/_book
     git checkout gh-pages
@@ -33,6 +38,7 @@ function deploy() {
     git add .
     git commit -a -m "update"
     git push origin gh-pages
+    git checkout doc
 }
 
 function main() {
