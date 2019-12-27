2fd13bc60fc6ea01af2395d53871883559d454bc
luoxiaodong <luoxiaodong@360.cn>
2019-12-27 19:45:38 +0800

    fix: rds cmd hscan

--- coms/rds/redis_cmd.go
+++ coms/rds/redis_cmd.go
@@ -281,7 +281,7 @@ func (r *Rds) HVals(key interface{}) (reply interface{}, err error) {
 
 func (r *Rds) HScan(key interface{}, cursor interface{}, opts ...interface{}) (reply interface{}, err error) {
 	args1 := mergeKeyAndArgs(cursor, opts...)
-	args := mergeKeyAndArgs(key, args1)
+	args := mergeKeyAndArgs(key, args1...)
 	return r.Do("HSCAN", args...)
 }
 
