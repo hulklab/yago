6681982415cf29290d3d2af1be4824b5751da737
LawSheldon <sheldon.law@outlook.com>
2020-04-22 11:20:18 +0800

    feat: add basetask runloopwhile method

--- base/basetask/task.go
+++ base/basetask/task.go
@@ -1,20 +1,41 @@
 package basetask
 
 import (
-	"github.com/hulklab/yago"
 	"time"
+
+	"github.com/hulklab/yago"
 )
 
-type BaseTask struct {
-}
+type BaseTask struct{}
 
-func (b BaseTask) RunLoop(handlerFunc yago.TaskHandlerFunc, interval ...time.Duration) {
+func (b BaseTask) RunLoop(handlerFunc func(), interval ...time.Duration) {
 	var intervalOne time.Duration
 	if len(interval) > 0 {
 		intervalOne = interval[0]
 	}
 	for {
 		handlerFunc()
+		if intervalOne > 0 {
+			time.Sleep(intervalOne)
+		}
+		select {
+		case <-yago.TaskCloseChan:
+			return
+		default:
+		}
+	}
+}
+
+//exit while return false, and continue while return true
+func (b BaseTask) RunLoopWhile(handlerFunc func() bool, interval ...time.Duration) {
+	var intervalOne time.Duration
+	if len(interval) > 0 {
+		intervalOne = interval[0]
+	}
+	for {
+		if !handlerFunc() {
+			return
+		}
 		if intervalOne > 0 {
 			time.Sleep(intervalOne)
 		}
