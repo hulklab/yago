6cc694ca660d4a377d66986f3b2cd25573a9f7e1
kowloonzh <kowloonzh@gmail.com>
2020-04-29 16:54:50 +0800

    feat: support UTC timestr

--- libs/date/date_test.go
+++ libs/date/date_test.go
@@ -1,15 +1,23 @@
 package date
 
 import (
-	"fmt"
 	"testing"
 )
 
 func TestStrToTime(t *testing.T) {
 
 	tm := StrToTime("2019-11-14 20:24:51", "Y-m-d H:i:s")
-	fmt.Println(tm.String())
+	if tm.String() != "2019-11-14 20:24:51 +0800 CST" {
+		t.Error("test StrToTime err", tm.String())
+	}
 
 	ts := StrToTimestamp("2019-11-14 20:24:51", TimeFormat)
-	fmt.Println(ts)
+	if ts != 1573734291 {
+		t.Error("test StrToTimestamp err", ts)
+	}
+
+	st := StrToTime("2019-11-14T12:24:51Z", "Y-m-dTH:i:sZ")
+	if st.Format(TimeFormat) != "2019-11-14 20:24:51" {
+		t.Error("test StrToTime with UTC str err", st.Format(TimeFormat))
+	}
 }
