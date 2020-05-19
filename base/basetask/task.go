ac09a8c78c0fab530c809d73d8c925ba846793d2
xdlaw <sheldon.law@outlook.com>
2020-05-19 21:13:33 +0800

    fix: remove runloopwhile

--- base/basetask/task.go
+++ base/basetask/task.go
@@ -149,27 +149,6 @@ HELL:
 	}
 }
 
-//exit while return false, and continue while return true
-func (b BaseTask) RunLoopWhile(handlerFunc func() bool, interval ...time.Duration) {
-	var intervalOne time.Duration
-	if len(interval) > 0 {
-		intervalOne = interval[0]
-	}
-	for {
-		if !handlerFunc() {
-			return
-		}
-		if intervalOne > 0 {
-			time.Sleep(intervalOne)
-		}
-		select {
-		case <-yago.TaskCloseChan:
-			return
-		default:
-		}
-	}
-}
-
 func (b BaseTask) Wait(cb func()) {
 	select {
 	case <-yago.TaskCloseChan:
