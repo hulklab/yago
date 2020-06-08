de844a55bd03c57a83c5ca2520aba78183f3cfc5
diligentyang <905407339@qq.com>
2020-06-08 18:24:41 +0800

    update es v6 => v7,add more test

--- coms/elastic/elastic.go
+++ coms/elastic/elastic.go
@@ -3,15 +3,15 @@ package elastic
 import (
 	"github.com/hulklab/yago"
 	"github.com/hulklab/yago/coms/logger"
-	"github.com/olivere/elastic"
+	"github.com/olivere/elastic/v7"
 	"log"
 )
 
-type ElasticV6 struct {
+type Elastic struct {
 	*elastic.Client
 }
 
-func InsV6(id ...string) *ElasticV6 {
+func Ins(id ...string) *Elastic {
 
 	var name string
 
@@ -24,6 +24,9 @@ func InsV6(id ...string) *ElasticV6 {
 	v := yago.Component.Ins(name, func() interface{} {
 		config := yago.Config.GetStringMap(name)
 		options := make([]elastic.ClientOptionFunc, 0)
+		var username string
+		var password string
+		logLevel := int64(4)
 
 		urls := yago.Config.GetStringSlice(name + ".urls")
 		if len(urls) < 1 {
@@ -32,39 +35,50 @@ func InsV6(id ...string) *ElasticV6 {
 
 		options = append(options, elastic.SetURL(urls...))
 
-		username := config["username"].(string)
-		password := config["password"].(string)
+		if config["username"] != nil {
+			username = config["username"].(string)
+		}
+
+		if config["password"] != nil {
+			password = config["password"].(string)
+		}
 
 		if username != "" && password != "" {
 			options = append(options, elastic.SetBasicAuth(username, password))
 		}
 
-		if infolog_enable, ok := config["infolog_enable"]; ok && infolog_enable.(bool) {
-			infolog := logger.Ins().Category("[ELASTIC_INFO " + name + "]")
-			options = append(options, elastic.SetInfoLog(infolog))
+		if config["level"] != nil {
+			logLevel = config["level"].(int64)
 		}
 
-		if tracelog_enable, ok := config["tracelog_enable"]; ok && tracelog_enable.(bool) {
+		if logLevel >= 6 {
 			tracelog := logger.Ins().Category("[ELASTIC_TRACE " + name + "]")
 			options = append(options, elastic.SetTraceLog(tracelog))
 		}
 
-		errlog := logger.Ins().Category("[ELASTIC_ERROR " + name + "]")
-		options = append(options, elastic.SetErrorLog(errlog))
+		if logLevel >= 4 {
+			infolog := logger.Ins().Category("[ELASTIC_INFO " + name + "]")
+			options = append(options, elastic.SetInfoLog(infolog))
+		}
 
-		if sniff_enable, ok := config["sniff_enable"]; ok && !sniff_enable.(bool) {
+		if logLevel >= 2 {
+			errlog := logger.Ins().Category("[ELASTIC_ERROR " + name + "]")
+			options = append(options, elastic.SetErrorLog(errlog))
+		}
+
+		if sniffEnable, ok := config["sniff_enable"]; ok && !sniffEnable.(bool) {
 			options = append(options, elastic.SetSniff(false))
 		}
 
 		client, err := elastic.NewClient(options...)
 		if err != nil {
-			//如果报错 no Elasticsearch node available  可能是用户名密码不正确，或者 sniff_enable没有置为false
+			//如果报错 no Elasticsearch node available  可能是用户名密码不正确，或者sniff_enable没有置为false
 			log.Fatalf("Fatal error url: %s", err)
 		}
-		return &ElasticV6{
+		return &Elastic{
 			Client: client,
 		}
 	})
 
-	return v.(*ElasticV6)
+	return v.(*Elastic)
 }
